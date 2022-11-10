cde56a841a3a0c8179117603002809cb9772d1f4
SaltyFingerz <denis.iannoukos@yandex.com>
2022-11-10 14:33:08 +0000

    enemies looking at player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private int EnemyHealth = 5;
+    public Transform target;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +16,21 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      
         print(EnemyHealth);
         if(EnemyHealth <= 0)
         {
             Destroy(gameObject);
         }
+
+        Vector3 targetPosition = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+        if(target != null)
+        {
+            
+            transform.LookAt(targetPosition);
+           // transform.position = Vector3.Lerp(transform.position, target.position, 0.01f);
+        }
+
     }
 
     public void ReduceHealth(int damage)
